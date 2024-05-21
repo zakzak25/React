cf89510fccc0d16b5e18c3a132831201ece8d9e8
zakzak25 <zbousba4@gmail.com>
2024-05-21 15:41:19 +0200

    created a react App

---
+++ src/components/Button/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './button.css';
+
+const Button = () => {
+    return(
+        <>
+          <button>Button</button>
+        </>
+    )
+}
+
+export default Button;
